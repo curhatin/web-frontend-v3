8f1c4c874986312c21f0cfad212bc2f5bcd91092
M Haidar Hanif <git@mhaidarhanif.com>
2018-11-28 17:02:34 +0700

    Fix typo on REDUX_DEVTOOLS_EXTENSION createStore

--- src/store.js
+++ src/store.js
@@ -1,8 +1,4 @@
-import {
-    createStore,
-    applyMiddleware,
-    compose
-} from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
 import rootReducer from "./reducer"
 // import createBrowserHistory from 'history/createBrowserHistory';
@@ -10,24 +6,21 @@ import rootReducer from "./reducer"
 // import reduxHistoryPushMiddleware from 'reduxHistoryPushMiddleware';
 
 const initialState = {
-    auth: {
-        isAuthenticated: false
-    },
-    post: {}
+  auth: {
+    isAuthenticated: false
+  },
+  post: {}
 }
 const middleware = [thunk]
 // const history = createBrowserHistory()
 // const reduxHistoryPush = reduxHistoryPushMiddleware(history, { matcher: '_FULFILLED$' });
 
-
-
 const store = createStore(
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware),
-    // applyMiddleware(reduxHistoryPush),
-    )
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(...middleware)),
+  // applyMiddleware(reduxHistoryPush),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store
